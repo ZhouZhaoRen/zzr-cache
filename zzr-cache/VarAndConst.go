256eca7c1e291f7fabb517222717eb833297a4c3
smallzhou <smallzhou@tencent.com>
2021-08-09 16:45:10 +0800

    增加文件操作和创建缓存

--- zzr-cache/VarAndConst.go
+++ zzr-cache/VarAndConst.go
@@ -7,5 +7,6 @@ import (
 const (
 	// 默认的过期时间，即插入数据时过期时间为默认的
 	DefaultExpiration time.Duration =0
+	TIME_FORMAT         = "2006-01-02 15:04:05"
 )
 
--- zzr-cache/cache.go
+++ zzr-cache/cache.go
@@ -1,7 +1,11 @@
 package zzr_cache
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"runtime"
 	"sync"
 	"time"
 )
@@ -14,6 +18,7 @@ type cache struct {
 	items             map[string]Item // 存储缓存的容器
 	rw                sync.RWMutex    // 读写锁
 	onEvicted         func(string, interface{})
+	janitor           janitor
 }
 
 // Set 往缓存中插入元素
@@ -162,4 +167,168 @@ func (c *cache) DeleteExpired() {
 	for _, v := range expiredItems {
 		c.onEvicted(v.key, v.value)
 	}
+}
+
+// SaveData 分为两个文件保存，一个保存全部的数据，一个保存当前的数据
+func (c *cache) SaveData(path ...string) error {
+	var (
+		file       *os.File
+		nowFile    *os.File
+		nowFileErr error
+		err        error
+	)
+
+	if len(path) > 1 {
+		fmt.Println("只能传入一个路径")
+		return fmt.Errorf("只能传入一个路径")
+	} else if len(path) == 1 {
+		// 判断路径是否存在，否则创建路径
+		if _, err := os.Stat(path[0]); os.IsNotExist(err) {
+			// 创建
+			err = os.MkdirAll(path[0], os.ModePerm)
+			if err != nil {
+				fmt.Printf("os.MkdirAll callee failed:%+v", err)
+				return err
+			}
+		}
+		nowFile, nowFileErr = os.OpenFile(fmt.Sprintf("%s\\%s.log", path[0], "nowData"), os.O_CREATE|os.O_WRONLY, 0600)
+		file, err = os.OpenFile(fmt.Sprintf("%s\\%s.log", path[0], "allData"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+	} else {
+		nowFile, nowFileErr = os.OpenFile(fmt.Sprintf("%s.log", "nowData"), os.O_CREATE|os.O_WRONLY, 0600)
+		file, err = os.OpenFile(fmt.Sprintf("%s.log", "allData"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+	}
+	// 先判断文件夹是否存
+	if nowFileErr != nil {
+		fmt.Printf("os.OpenFile callee failed:%+v", err)
+		return err
+	}
+	if err != nil {
+		fmt.Printf("os.OpenFile callee failed:%+v", err)
+		return err
+	}
+	c.rw.RLock()
+	defer c.rw.RUnlock()
+	// 对数据编码
+	content, _ := json.Marshal(c.items)
+	_, _ = file.Write([]byte(time.Now().Format(TIME_FORMAT)))
+	_, _ = file.Write(content)
+	_, _ = file.Write([]byte("\n"))
+	// 当前数据
+	_, _ = nowFile.Write(content)
+	fmt.Println("数据写结束")
+	return nil
+}
+
+// LoadFile 加载文件里面的数据到内存中
+func (c *cache) LoadFile(fileName string) error {
+	items := map[string]Item{}
+	datas, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("ioutil.ReadFile(fileName) error:", err)
+		return err
+	}
+	err = json.Unmarshal(datas, &items)
+	if err != nil {
+		fmt.Println("反序列化失败：", err)
+		return err
+	}
+	//
+	c.rw.Lock()
+	for k, v := range items {
+		value, found := c.items[k]
+		if !found || value.Expired() {
+			c.items[k] = v
+		}
+	}
+	c.rw.Unlock()
+	return nil
+}
+
+// Items 复制一份返回去
+func (c *cache) Items() map[string]Item {
+	c.rw.RLock()
+	defer c.rw.RUnlock()
+	m := make(map[string]Item, len(c.items))
+	now := time.Now().UnixNano()
+	for k, v := range c.items {
+		if v.Expiration > 0 && now > v.Expiration {
+			continue
+		}
+		m[k] = v
+	}
+	return m
+}
+
+func (c *cache) ItemCount() int {
+	c.rw.RLock()
+	defer c.rw.RUnlock()
+	return len(c.items)
+}
+
+func (c *cache) Flush() {
+	c.rw.Lock()
+	defer c.rw.Unlock()
+	c.items = map[string]Item{}
+}
+
+type janitor struct {
+	Interval time.Duration
+	stop     chan bool
+}
+
+func (j *janitor) Run(c *cache) {
+	ticker := time.NewTicker(j.Interval)
+	for {
+		select {
+		case <-ticker.C:
+			// 定时删除过期数据
+			_=c.SaveData()
+			c.DeleteExpired()
+		case <-j.stop:
+			ticker.Stop()
+			return
+		}
+	}
+}
+
+func stopInterval(c *cache) {
+	c.janitor.stop <- true
+}
+
+func RunJanitor(c *cache, ci time.Duration) {
+	j := janitor{
+		Interval: ci,
+		stop:     make(chan bool),
+	}
+	c.janitor = j
+	go j.Run(c)
+}
+
+//
+func New(defaultTime, cleanUpTime time.Duration) *Cache {
+	item := make(map[string]Item)
+	return newCacheWithJanitor(defaultTime, cleanUpTime, item)
+}
+
+// newCacheWithJanitor 新建一个缓存结构体并返回，同时启动结构体的定时任务
+func newCacheWithJanitor(defaultTime, cleanUpTime time.Duration, item map[string]Item) *Cache {
+	c := newCache(defaultTime, item)
+	C := &Cache{c}
+	if cleanUpTime > 0 {
+		RunJanitor(c, cleanUpTime)
+		runtime.SetFinalizer(C, stopInterval)
+	}
+	return C
+}
+
+// newCache 新建一个存储缓存的结构体
+func newCache(defaultTime time.Duration, item map[string]Item) *cache {
+	if defaultTime == 0 {
+		defaultTime = -1
+	}
+	c := &cache{
+		items:             item,
+		defaultExpiration: defaultTime,
+	}
+	return c
 }
